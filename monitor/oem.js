c207a081c67a68d75937a3a13f2a4a17b11389f6
Toby Ealden <toby.ealden@gmail.com>
2015-01-14 11:29:18 +0000

    Debugging on RPi

--- monitor/oem.js
+++ monitor/oem.js
@@ -7,94 +7,125 @@
   var delimiter = "\r\n";
   var eventEmitter = require('events').EventEmitter;
 
-  function emon(port) {
+  var deviceMappings = {
+    "emonTH": [
+      {
+        "name": "temperature",
+        "scale": 0.1
+      },
+      { "name": "external temp",
+        "scale": 0.1
+      },
+      { "name": "humidity",
+        "scale": 0.1
+      },
+      { "name": "voltage",
+        "scale": 0.1
+      }
+    ],
+    "emonTx": [
+      {
+        "name": "power1",
+        "scale": 1
+      },
+      {
+        "name": "power2",
+        "scale": 1
+      },
+      {
+        "name": "power3",
+        "scale": 1
+      },
+      {
+        "name": "power4",
+        "scale": 1
+      },
+      {
+        "name": "voltage",
+        "scale": 0.01
+      },
+      {
+        "name": "temperature",
+        "scale": 0.1
+      }
+    ]
+  }
+
+  function OEM(port) {
     eventEmitter.call(this);
 
     this._portName = port;
     this._serialPort = null;
+    this._configuredDevices = config.getLocal("oemDevices");
+    this._cachedData = {};
   }
 
-  emon.prototype = Object.create(eventEmitter.prototype);
-  emon.prototype.constructor = emon;
+  OEM.prototype = Object.create(eventEmitter.prototype);
+  OEM.prototype.constructor = OEM;
 
-  emon.prototype.start = function() {
+  OEM.prototype.start = function() {
     var self = this;
 
     this._serialPort = new serialModule.SerialPort(this._portName, { parser: serialModule.parsers.readline(delimiter), baudrate: 38400}, false);
 
     this._serialPort.open(function(err) {
       if (typeof err !== "undefined" && err !== null) {
-        console.log("emon - failed to open port " + self._portName + " - " + JSON.stringify(err));
+        console.log("OEM - failed to open port " + self._portName + " - " + JSON.stringify(err));
       } else {
-        utils.logger.info("emon - opened port");
+        utils.logger.info("OEM - opened port");
 
         self._serialPort.on("error", function(e) {
-          utils.logger.info("emon - port error: " + JSON.stringify(e));
+          utils.logger.info("OEM - port error: " + JSON.stringify(e));
         });
 
         self._serialPort.on("data", function (data) {
           if (typeof data !== "undefined" && data !== null) {
-            utils.logger.info("emon: " + data);
+            utils.logger.info("OEM: " + data);
             onDataReceived.call(self, data);
           }
         });
-
-        // Set 'poll' operating mode.
-        self._serialPort.write("K 2\r\n");
       }
     });
   };
 
-  var startPolling = function() {
-    if (this._timer === 0) {
-      this._timer = setInterval(poll.bind(this), config.get().cozirPollInterval*60*1000);
-    }
-  };
-
-  var poll = function() {
-    var self = this;
-
-    // ToDo - review sequencing.
-    setTimeout(function() { self._serialPort.write("Z\r\n"); }, 0);
-    setTimeout(function() { self._serialPort.write("T\r\n"); }, 250);
-    setTimeout(function() { self._serialPort.write("H\r\n"); }, 500);
-  };
-
-  var handleCO2 = function(data) {
-    var co2 = data.substr(2);
-    this.emit("co2",Date.now(),parseInt(co2));
-  };
-
-  var handleHumidity = function(data) {
-    var humidity = data.substr(2);
-    this.emit("humidity",Date.now(),parseInt(humidity)/10);
-  };
-
-  var handleTemperature = function(data) {
-    var temp = data.substr(2);
-    this.emit("temperature",Date.now(),(parseInt(temp) - 1000)/10);
-  };
-
   var onDataReceived = function(data) {
-    switch (data[1]) {
-      case "Z":
-        handleCO2.call(this,data);
-        break;
-      case "T":
-        handleTemperature.call(this,data);
-        break;
-      case "H":
-        handleHumidity.call(this,data);
-        break;
-      case "K":
-        startPolling.call(this);
-        break;
-      default:
-        utils.logger.info("ignoring data: " + data);
-        break;
+
+    var split = data.split(' ');
+    if (split.length < 2 || split[0] !== "OK") {
+      utils.logger.info("OEM - bad frame: " + data);
+    } else {
+      var nodeId = split[1];
+      if (this._configuredDevices.hasOwnProperty(nodeId)) {
+        var logDevice = this._configuredDevices[nodeId];
+        var deviceType = logDevice.type;
+        if (deviceMappings.hasOwnProperty(deviceType)) {
+          var deviceConfig = deviceMappings[deviceType];
+          var logObj = {};
+          var dataIndex = 2;
+          for (var i = 0, len = deviceConfig.length; i < len; i++) {
+            if (logDevice.log.hasOwnProperty(deviceConfig[i].name)) {
+              var dataItem = parseInt(split[dataIndex]) + parseInt(split[dataIndex+1])*256;
+              logObj[deviceConfig[i].name] = dataItem;
+            }
+          }
+
+          // Check if data has changed.
+          var jsonData = JSON.stringify(logObj);
+          if (jsonData === this._cachedData[nodeId]) {
+            utils.logger.info("OEM - data not changed for node " + nodeId);
+          } else {
+            this.emit("update",logObj);
+          }
+          this._cachedData[nodeId] = logObj;
+        } else {
+          utils.logger.info("OEM - no configuration for device type: " + deviceType);
+        }
+
+      } else {
+        utils.logger.info("OEM - ignoring data for node " + nodeId);
+      }
     }
   };
 
-  module.exports = emon;
-
+  module.exports = OEM;
 }());
